22070717751e9f636eb5f975eab7ce3074db5976
reshapedata <29032529@qq.com>
2023-09-01 09:03:19 +0000

    v1.1.1 添加了数据删除功能

---
+++ R/salary.R
@@ -0,0 +1,53 @@
+#' 处理逻辑
+#'
+#' @param input 输入
+#' @param output 输出
+#' @param session 会话
+#' @param dms_token 口令
+#'
+#' @return 返回值
+#' @export
+#'
+#' @examples
+#' salaryServer()
+salaryServer <- function(input,output,session,dms_token) {
+  var_text_hrv_voucher_year=tsui::var_numeric("text_hrv_voucher_year")
+  var_text_hrv_voucher_month=tsui::var_numeric("text_hrv_voucher_month")
+  var_text_hrv_voucher_environment=tsui::var_ListChoose1("text_hrv_voucher_environment")
+  
+  shiny::observeEvent(input$btn_salary,{
+    token=dms_token
+    FYear=var_text_hrv_voucher_year()
+    FMonth=var_text_hrv_voucher_month()
+    FExpenseOrgID=var_text_hrv_voucher_environment()
+    
+    
+    
+    #按承担组织清除--工资
+    
+    jhhrvvoucherpkg::stdsalary(token = token,FYear =FYear ,FMonth =FMonth ,FExpenseOrgID =FExpenseOrgID )
+    
+    
+    
+    jhhrvvoucherpkg::srcsalary(token = token,FYear =FYear ,FMonth =FMonth ,FExpenseOrgID =FExpenseOrgID )
+    
+    
+    jhhrvvoucherpkg::odssalary(token = token,FYear =FYear ,FMonth =FMonth ,FExpenseOrgID =FExpenseOrgID )
+    
+    #按--清除中间表
+    str <-".0"
+    FYear2=sprintf("%s%s",FYear ,str)
+    FMonth2=sprintf("%s%s",FMonth ,str)
+    
+    jhhrvvoucherpkg::srcmiddleTable(token = token,FYear =FYear2 ,FMonth =FMonth2 ,FExpenseOrgID =FExpenseOrgID )
+    
+   
+    tsui::pop_notice("工资相关数据已清除成功")
+    
+    
+    
+    
+    
+    
+  })
+}
---
+++ R/socialsecurity.R
@@ -0,0 +1,51 @@
+#' 处理逻辑
+#'
+#' @param input 输入
+#' @param output 输出
+#' @param session 会话
+#' @param dms_token 口令
+#'
+#' @return 返回值
+#' @export
+#'
+#' @examples
+#' socialsecurityServer()
+socialsecurityServer <- function(input,output,session,dms_token) {
+  var_text_hrv_voucher_year=tsui::var_numeric("text_hrv_voucher_year")
+  var_text_hrv_voucher_month=tsui::var_numeric("text_hrv_voucher_month")
+  var_text_hrv_voucher_environment=tsui::var_ListChoose1("text_hrv_voucher_environment")
+  
+  shiny::observeEvent(input$btn_socialsecurity,{
+    token=dms_token
+    FYear=var_text_hrv_voucher_year()
+    FMonth=var_text_hrv_voucher_month()
+    FExpenseOrgID=var_text_hrv_voucher_environment()
+    
+    
+    
+    #按承担组织清除--工资
+    
+    jhhrvvoucherpkg::stdsocialsecurity(token = token,FYear =FYear ,FMonth =FMonth ,FExpenseOrgID =FExpenseOrgID )
+    
+    
+    jhhrvvoucherpkg::srcsocialsecurity(token = token,FYear =FYear ,FMonth =FMonth ,FExpenseOrgID =FExpenseOrgID )
+    
+    jhhrvvoucherpkg::odssocialsecurity(token = token,FYear =FYear ,FMonth =FMonth ,FExpenseOrgID =FExpenseOrgID )
+    
+    #按--清除中间表
+    str <-".0"
+    FYear2=sprintf("%s%s",FYear ,str)
+    FMonth2=sprintf("%s%s",FMonth ,str)
+    
+    jhhrvvoucherpkg::srcmiddleTable(token = token,FYear =FYear2 ,FMonth =FMonth2 ,FExpenseOrgID =FExpenseOrgID )
+    
+    
+    tsui::pop_notice("社保相关数据已清除成功")
+    
+    
+    
+    
+    
+    
+  })
+}
